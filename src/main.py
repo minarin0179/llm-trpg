3d476e8712b4c57f221e4d8fbdba800f09bc9271
minarin0179 <55317109+minarin0179@users.noreply.github.com>
2024-10-29 07:52:11 +0000

    fix debate response

--- src/main.py
+++ src/main.py
@@ -44,6 +44,8 @@ shared_prompt = f"""
 GM_instruction = f"""
 あなたはTRPGのゲームマスターです.
 今から{GAME_SYSTEM}のシナリオを一緒に遊びましょう．
+「了解しました」などといった要求に対する応答は必要ありません．
+また出力にmarkdown記法を用いてはいけません．
 {scenario_text}
 {character_text}
 {shared_prompt}
@@ -52,8 +54,9 @@ GM_instruction = f"""
 assistants = [
     """
     あなたはTRPGのゲームマスターの補佐役です.
-    ゲームマスターである私のプレイヤーに対する，返信について問題がなければ「OK」とだけ返してください．
-    問題点があれば，その問題点を具体的に指摘し，改善案を提案してください．
+    ゲームマスターである私のプレイヤーに対する応答について参照するべきルールがあればそれを引用して補足してください．
+    また，私の応答が該当の則っていない場合は，修正方法を提案してください．
+    修正すべき点がなければ出力の最後に"OK"，修正するべき点があれば"NG"と返してください.
     ルールブックの内容は以下の通りです．
     {rulebook_text}
     """,
@@ -61,7 +64,7 @@ assistants = [
 
 messages = [
     {"role": "system", "content": GM_instruction},
-    {"role": "user", "content": "セッションを始めましょう"},
+    {"role": "user", "content": "それではセッションを始めましょう.まずはシナリオ概要の説明と導入をお願いします."},
 ]
 
 
@@ -85,83 +88,82 @@ def debug_print(text):
 
 
 def generate_debate_response() -> ChatCompletion:
-    temporal_response = client.chat.completions.create(
-        model="gpt-4o",
-        messages=messages,
-        tools=tools,
-    )
+    temporal_messages_for_gamemaster = messages.copy()
 
-    message = temporal_response.choices[0].message
-
-    tool_call = message.tool_calls[0] if message.tool_calls else None
-
-    if tool_call and tool_call.function.name == "diceroll":
-        arguments = json.loads(tool_call.function.arguments)
-        command = arguments.get("command")
-
-        diceroll_result = dicebot.exec(command)
-        show_diceroll_result(diceroll_result)
-        print("-"*30)
-
-        func_result = {
-            "role": "tool",
-            "content": json.dumps(diceroll_result),
-            "tool_call_id": temporal_response.choices[0].message.tool_calls[0].id,
-        }
-        # dicerollの際はtoolcallを履歴に追加
-        messages.append(temporal_response.choices[0].message.to_dict())
-        messages.append(func_result)
-
-        # toolcallしたら結果を入れて続きの応答を生成
-        return generate_debate_response()
-
-    # main loop
+    # feedback loop
     for i in range(MAX_FEEDBACK):
+        print(temporal_messages_for_gamemaster)
+        temporal_response = client.chat.completions.create(
+            model="gpt-4o",
+            messages=temporal_messages_for_gamemaster,
+            tools=tools,
+        )
         debug_print(temporal_response.choices[0].message.content)
+
+        if temporal_response.choices[0].message.content is None:
+            # toolcallでcontentがnullの時はダイスを振って追加で応答を生成
+            messages.append(temporal_response.choices[0].message.to_dict())
+            handle_tool_call(temporal_response)
+            return generate_debate_response()
+
+        # contentがnullじゃないときはtoolcallを外す
+        temporal_message = temporal_response.choices[0].message.to_dict()
+        temporal_message.pop("tool_calls", None)
+        temporal_messages_for_gamemaster.append(temporal_message)
+
         feedbacks = []
         for assistant in assistants:
             temporal_messages = [
                 {"role": "system", "content": assistant},
-                {"role": "user", "content": f"以下は直前のGMとプレイヤーのやり取りです{
+                {"role": "user", "content": f"以下は直近のGMとプレイヤーのやり取りです{
                     # messages[-2:]とtemporalresponseを結合してstringfyする
-                    stringfy_messages(messages[-2:] + [temporal_response.choices[0].message.to_dict()])}"
+                    stringfy_messages(temporal_messages_for_gamemaster[-2*(i+1)-1:])}"
                  },
             ]
 
-            # TODO: feedbackを会話履歴に残すべきか検証
-
+            print(temporal_messages)
             feedback_response = client.chat.completions.create(
                 model="gpt-4o",
                 messages=temporal_messages,
-                tools=tools,
+                # tools=tools, # feedbackの時はtoolcallを使わない
             )
 
             debug_print(f"feedback{i} : {
                         feedback_response.choices[0].message.content}")
             # TODO: reasoningも含めて出力
-            if feedback_response.choices[0].message.content == "OK":
+            if feedback_response.choices[0].message.content.endswith("OK"):
+                # TODO : 出力制御が上手くいってない jsonモードを試す
                 continue
 
             feedbacks.append(feedback_response)
 
         if not feedbacks:  # 全員がOKを出したら終了
+            final_response = temporal_response
             break
         # feedbackを踏まえてtemporalresponseを更新する
-        temporal_response = client.chat.completions.create(
+        temporal_messages_for_gamemaster.append(
+            {
+                "role": "user",
+                "content": f"""
+                前回の応答に対してフィードバックを与えるので，それらを踏まえて応答をやり直してください．
+                「再度やり直します」などの断りは不要です．
+                {"\n".join([
+                    f"feedback{i} : {feedback.choices[0].message}" for i, feedback in enumerate(feedbacks)
+                ])}
+                """}
+        )
+    else:  # 回数上限に達した場合は最終応答を生成
+        final_response = client.chat.completions.create(
             model="gpt-4o",
-            messages=[
-                *messages,
-                temporal_response.choices[0].message,
-                {"role": "user", "content": "前回の応答に対してフィードバックを与えるので，それらを踏まえて応答をやり直してください．"},
-                *[{"role": "user", "content": f"feedback{i} : {feedback.choices[0].message}"}
-                    for i, feedback in enumerate(feedbacks)],
-            ],
+            messages=temporal_messages_for_gamemaster,
             tools=tools,
         )
 
-        messages.append(temporal_response.choices[0].message.to_dict())
+    messages.append(final_response.choices[0].message.to_dict())
 
-    return temporal_response
+    print(f"GM : {final_response.choices[0].message.content}")
+    print("-"*30)
+    return final_response
 
 
 def generate_response(temporal: bool = False) -> ChatCompletion:
@@ -221,6 +223,28 @@ def save_session():
     print(f"セッション履歴が保存されました: {file_path}")
 
 
+def handle_tool_call(response):
+    message = response.choices[0].message
+    tool_call = message.tool_calls[0] if message.tool_calls else None
+
+    if tool_call and tool_call.function.name == "diceroll":
+        arguments = json.loads(tool_call.function.arguments)
+        command = arguments.get("command")
+
+        diceroll_result = dicebot.exec(command)
+        show_diceroll_result(diceroll_result)
+        print("-"*30)
+
+        func_result = {
+            "role": "tool",
+            "content": json.dumps(diceroll_result),
+            "tool_call_id": response.choices[0].message.tool_calls[0].id,
+        }
+
+        messages.append(func_result)
+    return response
+
+
 if __name__ == "__main__":
     # initial response
     response = generate_response()
@@ -234,6 +258,6 @@ if __name__ == "__main__":
         messages.append({"role": "user", "content": user_input_text})
         # response = generate_response()
         response = generate_debate_response()
-        print(response.choices[0].message.content)
 
+        handle_tool_call(response)
     # TODO エラーで中断した時用にmessagesを保存して再開する仕組みを作る
