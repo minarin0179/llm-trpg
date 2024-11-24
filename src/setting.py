8286634d46ad885a97d8117f547c29c7e98a72e8
minarin0179 <55317109+minarin0179@users.noreply.github.com>
2024-11-24 15:48:54 +0000

    change default scenario to kisagarieki

--- src/setting.py
+++ src/setting.py
@@ -1,6 +1,6 @@
 
 # ゲームの設定
 GAME_SYSTEM = "エモクロアTPRG"
-SCENARIO_PATH = "scenario/hasshakusama_scenario.txt"
+SCENARIO_PATH = "scenario/kisaragieki.md"
 CHARACTER_PATH = "character/意欲的な新米探偵.txt"
 RULEBOOK_PATH = "rulebook/emoklore.txt"
